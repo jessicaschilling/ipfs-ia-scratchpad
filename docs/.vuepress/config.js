5e52206764f045991f016ac0940d277baa3b3953
Jessica Schilling <jessica@protocol.ai>
2020-09-18 12:04:26 -0600

    Add some boilerplate

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'IPFS',
+  description: 'Just playing around',
+  themeConfig: {
+    search: false,
+    nav: [
+      {
+        text: 'Languages',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: 'Chinese', link: '/language/chinese/' },
+          { text: 'Japanese', link: '/language/japanese/' }
+        ]
+      }
+    ]
+  }
+}
