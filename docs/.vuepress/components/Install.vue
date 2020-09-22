25882144544ded18308b93c1cabbc5372e24e3c2
Jessica Schilling <jessica@protocol.ai>
2020-09-22 13:22:54 -0600

    Add blank modals

--- docs/.vuepress/components/Install.vue
+++ docs/.vuepress/components/Install.vue
@@ -14,28 +14,56 @@
     <div class="flex">
       <div class="bg-white mr3 pa3 tc f4">
         <div class="lh-title f4 fw3 mb3">IPFS Desktop for an easy-to-use GUI</div>
-        <a class="f5 link dim ba bw1 ph4 pv3 mb2 br2 dib dark-gray ml2" href="#0">Details</a>
+        <a class="f5 link dim ba bw1 ph4 pv3 mb2 br2 dib dark-gray ml2" href="#modal-desktop">Details</a>
       </div>
       <div class="bg-white mr3 pa3 tc f4">
         <div class="lh-title f4 fw3 mb3">IPFS Companion for integrated IPFS in your browser</div>
-        <a class="f5 link dim ba bw1 ph4 pv3 mb2 br2 dib dark-gray ml2" href="#0">Details</a>
+        <a class="f5 link dim ba bw1 ph4 pv3 mb2 br2 dib dark-gray ml2" href="#modal-companion">Details</a>
       </div>
       <div class="bg-white pa3 tc f4">
         <div class="lh-title f4 fw3 mb3">IPFS Cluster for IPFS at scale</div>
-        <a class="f5 link dim ba bw1 ph4 pv3 mb2 br2 dib dark-gray ml2" href="#0">Details</a>
+        <a class="f5 link dim ba bw1 ph4 pv3 mb2 br2 dib dark-gray ml2" href="#modal-cluster">Details</a>
       </div>
     </div>
+
+    <div id="modal-desktop" class="modal-window fixed top-0 right-0 bottom-0 left-0 z-999">
+      <div class="absolute pa4 bg-white">
+        <a href="#install" title="Close" class="mt3 mr3 tr absolute top-0 right-0 no-underline f4">X</a>
+        <h1 class="mt2 f2 w-90">Desktop</h1>
+        <div>A CSS-only modal based on the :target pseudo-class. Hope you find it helpful.</div>
+        <a href="https://aminoeditor.com" target="_blank">👉 Amino: Live CSS Editor for Chrome</a>
+        </div>
+    </div>
+
+    <div id="modal-companion" class="modal-window fixed top-0 right-0 bottom-0 left-0 z-999">
+      <div class="absolute pa4 bg-white">
+        <a href="#install" title="Close" class="mt3 mr3 tr absolute top-0 right-0 no-underline f4">X</a>
+        <h1 class="mt2 f2 w-90">Companion</h1>
+        <div>A CSS-only modal based on the :target pseudo-class. Hope you find it helpful.</div>
+        <a href="https://aminoeditor.com" target="_blank">👉 Amino: Live CSS Editor for Chrome</a>
+        </div>
+    </div>
+
+    <div id="modal-cluster" class="modal-window fixed top-0 right-0 bottom-0 left-0 z-999">
+      <div class="absolute pa4 bg-white">
+        <a href="#install" title="Close" class="mt3 mr3 tr absolute top-0 right-0 no-underline f4">X</a>
+        <h1 class="mt2 f2 w-90">Cluster</h1>
+        <div>A CSS-only modal based on the :target pseudo-class. Hope you find it helpful.</div>
+        <a href="https://aminoeditor.com" target="_blank">👉 Amino: Live CSS Editor for Chrome</a>
+        </div>
+    </div>
+
     <p class="mt4 lh-title f4 ">
       Or, if you're not sure which flavor of IPFS is right for you, take this short quiz.
     </p>
     <div class="flex items-center justify-between bg-white pa3 f4">
       <label for="diagnostic">What do you want to do?</label>
       <select name="diagnostic" id="diagnostic">
-        <option value="volvo">Use IPFS in my browser</option>
-        <option value="saab">Store my own personal files</option>
-        <option value="mercedes">Store files at CDN scale</option>
-        <option value="audi">Store files at a big-data scale</option>
-        <option value="audi">Build apps or services on IPFS</option>
+        <option value="browser">Use IPFS in my browser</option>
+        <option value="files-personal">Store my own personal files</option>
+        <option value="files-cdn">Store files at CDN scale</option>
+        <option value="files-bigdata">Store files at a big-data scale</option>
+        <option value="build">Build apps or services on IPFS</option>
       </select>
     </div>
     <p class="tc lh-title mt4 f4 fw5">
@@ -50,5 +78,26 @@
 </template>
 
 <style lang="stylus" scoped>
-// css/stylus here or in global stylus file
+.modal-window {
+  background-color: rgba(0, 0, 0, 0.25);
+  visibility: hidden;
+  opacity: 0;
+  pointer-events: none;
+  transition: all 0.3s;
+  &:target {
+    visibility: visible;
+    opacity: 1;
+    pointer-events: auto;
+  }
+  &>div {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+
+.modal-window div:not(:last-of-type) {
+  margin-bottom: 15px;
+}
+
 </style>
