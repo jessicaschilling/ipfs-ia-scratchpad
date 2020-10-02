d417be52344e47df8949809c5e0a8857f5e8b366
Jessica Schilling <jessica@protocol.ai>
2020-10-02 12:13:31 -0600

    Internal tests: More nav tweaks

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,7 @@ module.exports = {
     logo: '/assets/img/ipfs-logo.svg',
     search: false,
     nav: [
-      { text: 'About IPFS', link: '/#about' },
+      { text: 'About', link: '/#about' },
       { text: 'Why IPFS?', link: '/#why' },
       { text: 'Docs', link: 'https://docs.ipfs.io' },
       { text: 'Blog & News', link: '/blog-news' },
@@ -22,11 +22,13 @@ module.exports = {
         text: 'Resources',
         items: [
           { text: 'Discussion/Support Forum', link: 'https://docs.ipfs.io' },
-          { text: 'Community Resources', link: 'https://docs.ipfs.io/community' },
           { text: 'ProtoSchool', link: 'https://proto.school' },
-          { text: 'IPFS Repo Directory', link: '/does/not/exist' },
+          { text: 'IPFS Repos', link: '/does/not/exist' },
+          { text: 'IPFS Community Guide', link: 'https://docs.ipfs.io/community' },
+          { text: 'How to Contribute', link: 'https://docs.ipfs.io/community/contribute/ways-to-contribute/' },
           { text: 'Newsletter', link: '/#newsletter' },
-          { text: 'Press Kit', link: '/press' }
+          { text: 'Press Kit', link: '/press' },
+          { text: 'Core Team', link: 'https://github.com/ipfs/team-mgmt/blob/master/TEAMS_ROLES_STRUCTURES.md' }
         ]
       },
       { text: 'INSTALL', link: '/#install' }
