a5997e39d44482a813250ec21a4b121dc62aa6c3
Jessica Schilling <jessica@protocol.ai>
2020-10-02 12:02:51 -0600

    Internal tests: top nav rearranging

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,19 +16,19 @@ module.exports = {
     nav: [
       { text: 'About IPFS', link: '/#about' },
       { text: 'Why IPFS?', link: '/#why' },
-      { text: 'Who\'s Using IPFS?', link: '/#who' },
+      { text: 'Docs', link: 'https://docs.ipfs.io' },
+      { text: 'Blog & News', link: '/blog-news' },
       {
-        text: 'Docs & Resources',
+        text: 'Resources',
         items: [
-          { text: 'Docs', link: 'https://docs.ipfs.io' },
           { text: 'Discussion/Support Forum', link: 'https://docs.ipfs.io' },
           { text: 'Community Resources', link: 'https://docs.ipfs.io/community' },
           { text: 'ProtoSchool', link: 'https://proto.school' },
           { text: 'IPFS Repo Directory', link: '/does/not/exist' },
-          { text: 'Newsletter', link: '/#newsletter' }
+          { text: 'Newsletter', link: '/#newsletter' },
+          { text: 'Press Kit', link: '/press' }
         ]
       },
-      { text: 'Blog & News', link: '/blog-news' },
       { text: 'INSTALL', link: '/#install' }
     ]
   }
