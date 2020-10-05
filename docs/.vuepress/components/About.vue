2e19bccdc4f2d2b0e24d437a35e8317a7deaefbe
Jessica Schilling <jessica@protocol.ai>
2020-10-05 17:12:51 -0600

    Internal tests: initial tweaks to "About" section

--- docs/.vuepress/components/About.vue
+++ docs/.vuepress/components/About.vue
@@ -2,12 +2,19 @@
   <div class="center mw8 mv5">
     <h2 class="f1 ma0 pb3 bw0">About IPFS</h2>
     <p class="mt3 lh-title f3 fw4">
-      <span class="fw6">The InterPlanetary File System is many things.</span> It's a protocol for storing and sharing online content without relying on big corporations. It's a cutting-edge technology with a modern <a href="https://docs.ipfs.io/project/history/" target="_blank">origin story</a> and a worldwide <a href="https://github.com/ipfs/ipfs" target="_blank">open-source community</a>. It's the underpinning of <a href="https://filecoin.io" target="_blank">Filecoin</a>. But at its heart, it's here to make the web better for generations to come.
+      <span class="fw6">The InterPlanetary File System is many things.</span> It's a protocol for storing and providing data without relying on big corporations. It's the underpinning of <a href="https://filecoin.io" target="_blank">Filecoin</a>. It's a cutting-edge technology with a modern <a href="https://docs.ipfs.io/project/history/" target="_blank">origin story</a> and a worldwide <a href="https://github.com/ipfs/ipfs" target="_blank">open-source community</a>. But at its heart, it's here to make the web better for generations to come.
     </p>
-    <p class="mt5 lh-title f4">
-      Get the basics on what IPFS is and how it works in just three minutes.
-    </p>
-    <img class="mb3" src="/assets/img/video-placeholder.jpg">
+    <div class="flex mt5 mb5">
+      <div class="mr4 w-40">
+        <h3 class="f3 ma0 pa0">
+        Get the basics
+        </h3>
+        <p class="lh-title f4">
+          This three-minute summary explains what IPFS is, the essentials of how it works, and how it has the potential to revolutionize the web.
+        </p>
+      </div>
+      <img class="w-60" src="/assets/img/video-placeholder.jpg">
+    </div>
     <p class="mt5 lh-title f4">
       Explore this pull-apart diagram to find out how IPFS fits into the apps and services that are making tomorrow's distributed web a reality today.
     </p>
@@ -18,7 +25,7 @@
       </h3>
       <div class="flex mt2 justify-between">
         <div class="w-33 mr4">
-          <p class="lh-title">Try all-levels, interactive tutorials on a variety of distributed-web topics.</p>
+          <p class="lh-title">Try all-levels, interactive tutorials on a variety of distributed web topics.</p>
           <a class="fw7" href="https://proto.school/tutorials?course=ipfs" target="_blank">Visit ProtoSchool&nbsp;&nbsp;&gt;&gt;</a>
         </div>
         <div class="w-33 mr4">
