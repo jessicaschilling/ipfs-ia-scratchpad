ab25ff40716b655581ac2b469898bb0acfeec136
Jessica Schilling <jessica@protocol.ai>
2020-10-05 17:02:09 -0600

    Internal tests: Rework "why" section

--- docs/.vuepress/components/Why.vue
+++ docs/.vuepress/components/Why.vue
@@ -4,30 +4,71 @@
   <div class="center mw8 mv5">
     <h2 class="f1 ma0 pb3 bw0">Why IPFS?</h2>
     <p class="mt3 lh-title f3 fw4">
-      IPFS isn't just for storing and sharing files. With baked-in proof of authenticity, censorship and disaster resilience, and easy integration with both today's web and tomorrow's tech, organizations in industries around the globe are leveraging IPFS and its active <a href="https://docs.ipfs.io/community/" target="_blank">community</a>.
+      IPFS isn't just for storing and providing files. With baked-in <a href="does/not/exist" target="_blank">proof of authenticity</a>, <a href="does/not/exist" target="_blank">censorship and disaster resilience</a>, and <a href="does/not/exist" target="_blank">easy integration</a> with both today's web and tomorrow's tech, <a href="does/not/exist" target="_blank">organizations</a> in industries around the globe are leveraging IPFS and its active <a href="https://docs.ipfs.io/community/" target="_blank">community</a>.
     </p>
-    <div class="flex mt4 mb5">
-      <div class="bg-white w-40 mr4 pa4 tc f4">
-        <div class="fw5 f2">116K+</div>
-        <div>Unique network nodes</div>
+    <div class="flex mt4 mb5 montserrat">
+      <div class="bg-white charcoal-muted w-33 mr4 pa4 tc f4">
+        <h3 class="f1 ma0 mb2">1M+</h3>
+        <div>Unique IPFS users per week</div>
       </div>
-      <div class="bg-white w-40 mr4 pa4 tc f4">
-        <div class="fw5 f2">123</div>
-        <div>Number of collabs</div>
+      <div class="bg-white charcoal-muted w-33 mr4 pa4 tc f4">
+        <h3 class="f1 ma0 mb2">~500TB</h3>
+        <div>IPFS gateway traffic per week</div>
       </div>
-      <div class="bg-white w-40 pa4 tc f4">
-        <div class="fw5 f2">123</div>
-        <div>Collab benefit realized</div>
+      <div class="bg-white charcoal-muted w-33 pa4 tc f4">
+        <h3 class="f1 ma0 mb2">125+</h3>
+        <div>Organizations building on IPFS</div>
       </div>
     </div>
-    <p class="mt4 lh-title f4">
-      This two-minute video shows who can benefit from IPFS (including some of our big-name collabs), and how.
+    <div class="flex mt4 mb5">
+      <div class="mr4 w-40">
+        <h3 class="f3 ma0 pa0">
+        Real-world impact
+        </h3>
+        <p class="lh-title f4">
+          Get a two-minute overview of how IPFS can directly benefit some of the future's most vital industries &mdash; and meet some of the makers who are already realizing impact at a global scale.
+        </p>
+      </div>
+      <img class="w-60" src="/assets/img/video-placeholder.jpg">
+    </div>
+    <h3 class="f3 ma0 pa0">
+    A global ecosystem
+    </h3>
+    <p class="lh-title f4 mb1">
+      Organizations and platforms of all sizes are already utilizing IPFS to bring the power of the distributed web to every industry imaginable.
     </p>
-    <img src="/assets/img/video-placeholder.jpg">
-    <p class="mt5 lh-title f4 mb1">
-      Click to explore who's building on IPFS, their industries and the types of things they're creating, and the tool sets they're using in conjunction with IPFS.
-    </p>
-    <img src="/assets/img/diagram-ecosystem.png">
+    <div class="bg-white pa3 mt3 mb4 f4">
+      <div class="f5 h4 items-center lh-title flex justify-between pb3">
+        <div class="mr3 f2 teal-muted">&lt;</div>
+        <div class="ph4 flex items-center h-100 mr3 charcoal-muted br b--snow"><img class="desaturate o-50 w3" src="/assets/img/ecosystem-netflix.png"></div>
+        <div class="ph4 flex items-center h-100 mr3 charcoal-muted br b--snow"><img class="desaturate o-50 w3" src="/assets/img/ecosystem-brave.png"></div>
+        <div class="ph4 flex items-center h-100 mr3 charcoal-muted br b--snow"><img class="desaturate o-50 w3" src="/assets/img/ecosystem-opera.png"></div>
+        <div class="ph4 flex items-center h-100 mr3 charcoal-muted br b--snow"><img class="desaturate o-50 w3" src="/assets/img/ecosystem-microsoft.png"></div>
+        <div class="ph4 flex items-center h-100 mr3 charcoal-muted br b--snow"><img class="desaturate o-50 w3" src="/assets/img/ecosystem-cloudflare.png"></div>
+        <div class="ph4 flex items-center h-100 mr3 charcoal-muted br b--snow"><img class="desaturate o-50 w3" src="/assets/img/ecosystem-guix.png"></div>
+        <div class="ph4 flex items-center h-100 mr3 charcoal-muted"><img class="desaturate o-30 w3" src="/assets/img/ecosystem-kiwix.png"></div>
+        <div class="mr3 f2 teal-muted">&gt;</div>
+      </div>
+      <div class="f5 tr lh-title flex justify-between">
+        <div>
+          <label class="mr1" for="ecosystem-type">View</label>
+          <select name="ecosystem-type" id="ecosystem-type" class="charcoal">
+            <option value="integrations-collabs">Integrations &amp; collaborations</option>
+            <option value="ecommerce">Ecommerce</option>
+            <option value="content">Content providers</option>
+            <option value="nft">Non-fungible tokens</option>
+            <option value="defi">DeFi</option>
+            <option value="exchange">Exchange</option>
+            <option value="social-media">Social media</option>
+            <option value="data-markets">Data markets</option>
+            <option value="persistence">Persistence</option>
+            <option value="governance">Governance</option>
+            <option value="identity">Identity</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+        <a class="fw7" href="/does/not/exist" id="install">Explore the entire landscape&nbsp;&nbsp;&gt;&gt;</a></div>
+    </div>
     <div class="next-steps bg-white pa4 ba bw1 br3 b--gray-muted mt5">
       <h3 class="f3 ma0 pa0">
         Learn more
@@ -53,5 +94,7 @@
 </template>
 
 <style lang="stylus" scoped>
-// css/stylus here or in global stylus file
+img.desaturate {
+	filter: grayscale(100%);
+}
 </style>
