51b8f974c26ccdba2fde848559a5f7fa94cf414a
Jessica Schilling <jessica@protocol.ai>
2020-11-16 13:24:46 -0700

    Add visual design disclaimers

--- docs/.vuepress/components/Hero.vue
+++ docs/.vuepress/components/Hero.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="bg-washed-blue tc pt6 pb5">
-    <h1 class="f1 mv4">IPFS is ...</h1>
-    <div class="f2 mt2 h2 mb5 charcoal-muted tc relative">
-      <div class="fader absolute w-100">Decentralized file storage + delivery</div>
-      <div class="fader absolute w-100">A protocol for building peer-to-peer apps</div>
-      <div class="fader absolute w-100">A path to a permanent, resilient web</div>
-      <div class="fader absolute w-100">The next generation of the internet</div>
+  <div>
+    <div class="bg-yellow-muted db pt5 pb2 ph3 f6">
+      <strong>NOTE:</strong> This site is a lightly formatted wireframe of <a href="https://docs.google.com/document/d/1ni0kQNTLJ8VcpCu-zvpyWceZ3qbZWTJSJVRLCsOxvbY/edit#" class="charcoal underline" target="_blank">this IA outline</a> and should <em>not</em> be considered an example of visual design.
     </div>
-    <div class="tc">
-      <a class="button-secondary f4 link glow o-80 ba bw1 ph4 pv3 mb2 br2 dib mr2" href="#why">Why IPFS?</a>
-      <a class="button-primary f4 link glow o-80 ba bw1 ph4 pv3 mb2 br2 dib b--teal white bg-teal ml2" id="about" href="#install">Get Started</a>
+    <div class="bg-washed-blue tc pt4 pb5">
+      <h1 class="f1 mv4">IPFS is ...</h1>
+      <div class="f2 mt2 h2 mb5 charcoal-muted tc relative">
+        <div class="fader absolute w-100">Decentralized file storage + delivery</div>
+        <div class="fader absolute w-100">A protocol for building peer-to-peer apps</div>
+        <div class="fader absolute w-100">A path to a permanent, resilient web</div>
+        <div class="fader absolute w-100">The next generation of the internet</div>
+      </div>
+      <div class="tc">
+        <a class="button-secondary f4 link glow o-80 ba bw1 ph4 pv3 mb2 br2 dib mr2" href="#why">Why IPFS?</a>
+        <a class="button-primary f4 link glow o-80 ba bw1 ph4 pv3 mb2 br2 dib b--teal white bg-teal ml2" id="about" href="#install">Get Started</a>
+      </div>
     </div>
   </div>
 </template>
--- docs/.vuepress/theme/components/Page.vue
+++ docs/.vuepress/theme/components/Page.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
-    <header class="bg-washed-blue pt5">
+    <div class="bg-yellow-muted db pt5 pb2 ph3 f6">
+      <strong>NOTE:</strong> This site is a lightly formatted wireframe of <a href="https://docs.google.com/document/d/1ni0kQNTLJ8VcpCu-zvpyWceZ3qbZWTJSJVRLCsOxvbY/edit#" class="charcoal underline" target="_blank">this IA outline</a> and should <em>not</em> be considered an example of visual design.
+    </div>
+    <header class="bg-washed-blue">
       <Content slot-key="header" class="center mw8 pa4"/>
     </header>
     <main class="page sans-serif">
